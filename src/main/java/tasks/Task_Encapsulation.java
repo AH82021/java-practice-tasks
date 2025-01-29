0016a5feb811df10ababec14373c32743dfd1acc
Jeena Noori <jeenanoori92@gmail.com>
2025-01-29 01:28:31 -0500

    provide BankAccount class

--- src/main/java/tasks/Task_Encapsulation.java
+++ src/main/java/tasks/Task_Encapsulation.java
@@ -54,6 +54,44 @@ package tasks;
  *
  * No solution is provided here; you must implement the logic as an exercise.
  */
+
+class BankAccount{
+    private String accountNumber;
+    private double balance;
+
+    public BankAccount() {
+    }
+
+    public BankAccount(String accountNumber, double balance) {
+        this.accountNumber = accountNumber;
+        this.balance = balance;
+    }
+
+
+    public String getAccountNumber() {
+        return accountNumber;
+    }
+
+    public void setAccountNumber(String accountNumber) {
+        this.accountNumber = accountNumber;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+
+    @Override
+    public String toString() {
+        return "BankAccount{" +
+                "accountNumber='" + accountNumber + '\'' +
+                ", balance=" + balance +
+                '}';
+    }
+}
 public class Task_Encapsulation {
 
     public static void main(String[] args) {
