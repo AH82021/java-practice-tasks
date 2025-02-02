153aa795e42d8e64916c3be52afee807e94ffaba
mohammasarwary <147075585+mohammasarwary@users.noreply.github.com>
2025-02-02 10:27:29 -0500

    add bankaccount class creat variable and make getter and seter

---
+++ src/main/java/tasks/BankAccount.java
@@ -0,0 +1,19 @@
+package tasks;
+
+public class BankAccount {
+    private String accountNumber;
+    private double balance;
+    public void setAccountNumber(){
+        this.accountNumber=accountNumber;
+    }
+    public void setBalance(){
+        this.balance=balance;
+    }
+    public String getAccountNumber(){
+        return accountNumber;
+    }
+    public double getBalance() {
+        return balance;
+
+    }
+}
