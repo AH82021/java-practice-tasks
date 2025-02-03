3f641e768949eee4ec4b4e6de71fe98868aaa260
mohammasarwary <147075585+mohammasarwary@users.noreply.github.com>
2025-02-02 19:34:02 -0500

    add bankaccount a methde to deposit and withraw mony

--- src/main/java/tasks/BankAccount.java
+++ src/main/java/tasks/BankAccount.java
@@ -6,6 +6,19 @@ public class BankAccount {
     public void setAccountNumber(){
         this.accountNumber=accountNumber;
     }
+    public BankAccount(){
+
+    }
+    public void deposit(double amount){
+        balance +=amount;
+        System.out.println("deposit"+ amount +"remaining balance"+balance);
+    }
+    public void withrawbalance(double amount){
+        if (balance>=amount){
+            balance-=amount;
+            System.out.println("withraw amount"+amount+"remainig balance"+balance);
+        }
+    }
     public void setBalance(){
         this.balance=balance;
     }
@@ -16,4 +29,6 @@ public class BankAccount {
         return balance;
 
     }
+
+
 }
